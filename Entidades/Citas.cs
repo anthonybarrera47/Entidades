9c64e4a9783e060d28fd20a342746e58367f5727
anthonybarrera47 <anthony_barrera@ucne.edu.do>
2020-06-03 20:42:19 -0400

    Agregando entidades a efcore

---
+++ Entidades/Citas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Citas : CamposEstandar
+    {
+        [Key]
+        public int CitaID { get; set; }
+        public Usuarios Cliente { get; set; }
+        public Negocios Negocio{ get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public string CodigoComprobacion { get; set; }
+        public EstadoCita Estado { get; set; }
+        public virtual List<CitasDetalle> Details { get; set; }
+
+        public enum EstadoCita
+        {
+            EnProceso = 0,
+            Finalizado = 1
+        }
+
+    }
+}
---
+++ Entidades/Contexto.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entidades
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Citas> Citas{ get; set; }
+        public DbSet<Ciudad> Ciudad{ get; set; }
+        public DbSet<Empleados> Empleados{ get; set; }
+        public DbSet<Etiquetas> Etiquetas { get; set; }
+        public DbSet<FotosProductos> FotosProductos{ get; set; }
+        public DbSet<Horarios> Horarios { get; set; }
+        public DbSet<Mensajes> Mensaje { get; set; }
+        public DbSet<Negocios> Negocios{ get; set; }
+        public DbSet<Productos> Productos{ get; set; }
+        public DbSet<Usuarios> Usuarios{ get; set; }
+        public DbSet<Ventas> Ventas{ get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=PromYourSelf;Trusted_Connection=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
